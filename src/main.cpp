43e3f89f30d6880d507ec2191a5278c1facdfd3a
Klemen <roxor1992@gmail.com>
2019-02-11 21:49:27 +0100

    Added new library for matrixses

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 #include <random>
 #include <limits>
 
-#include <xtensor/xarray.hpp>
-#include <xtensor/xio.hpp>
-#include <xtensor/xrandom.hpp>
+#include <Eigen/Dense>
 
 #include <cxxopts.hpp>
 
 using namespace std;
-using namespace xt;
+using namespace Eigen;
 
 enum Test {
 	ONE = 1,
@@ -17,44 +15,6 @@ enum Test {
 };
 
 int main(int argc, char* argv[]) {
-	// Example of init
-	cout << endl << endl;
-	xarray<int> arr {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	cout << arr.shape()[0] << endl;
-	xarray<int> r = argwhere(arr < 5);
-	cout << r.data() << endl;
-	arr.reshape({3, 3});
-	cout << arr.shape().size() << endl;
-	cout << arr << endl;
-
-	// Example of operations
-	cout << endl << endl;
-	cout << arr + arr << endl;
-	cout << arr * arr << endl;
-	cout << arr / arr << endl;
-
-	// Example for using randome engine
-	cout << endl << endl;
-	auto rnd1 = random::get_default_random_engine();
-	auto rnd2 = random::get_default_random_engine();
-	rnd1.seed(123);
-	rnd2.seed(123);
-	cout << random::rand<double>({3, 3}, -10, 10, rnd1) << endl;
-	cout << random::rand<double>({3, 3}, -10, 10, rnd2) << endl;
-	cout << random::rand<double>({4, 4, 4, 4}) << endl;
-
-	// random generator
-	cout << endl << endl;
-	auto rand1 = minstd_rand0(123);
-	auto rand2 = minstd_rand0(123);
-	auto dist = uniform_int_distribution(0, 10);
-	for (int i = 0; i < 10; ++i) {
-		cout << dist(rand1) << " " << dist(rand2) << " | ";
-	}
-	cout << endl;
-	cout << random::rand<double>({2, 2}, -10, 10, rand1) << endl;
-	cout << random::rand<double>({2, 2}, -10, 10, rand2) << endl;
-
 	// infinity
 	cout << endl << endl;
 	auto infd = numeric_limits<double>::infinity();
@@ -68,9 +28,20 @@ int main(int argc, char* argv[]) {
 	cout << -1 * infd << endl;
 	cout << -1 * infi << endl;
 
+	// Enum test
 	cout << endl << endl;
 	cout << ONE << endl;
 	cout << NONE << endl;
+
+	// Eigen test
+	cout << endl << endl;
+	MatrixXd m = MatrixXd::Random(3,3);
+	m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+	cout << "m =" << endl << m << endl;
+	VectorXd v(3);
+	v << 1, 2, 3;
+	cout << "m * v =" << endl << m * v << endl;
+	cout << "m .* v =" << endl << m.array().colwise() * v.array() << endl;
 
 	return 0;
 }
